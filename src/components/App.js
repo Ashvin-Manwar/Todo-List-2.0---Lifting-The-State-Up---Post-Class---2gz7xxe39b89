0e76794f818623b22958b765e2dcf482d1ca49c8
Ashvin Manwar <manwar.ashvin@gmail.com>
2023-03-03 20:44:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,19 @@ import Header from './Header';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../styles/App.css';
 const App = () => {
-
+   const [state, setState] = useState("INBOX");
+  function doChange(inp){
+     setState(inp);
 
   return (
     <div id="main">
     <Header />
       <div className='row'>
         <div className='col-md-2'>
-          <SideNav change={}/>
+          <SideNav change={doChange}/>
         </div>
         <div className='col-md-10'>
-          <MainSection active = {}/>
+          <MainSection active = {state}/>
         </div>
       </div>
     </div>
